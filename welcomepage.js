16b3938668a5bad689634bd74059097d94caacac
Kendall <vedaker@st.ug.edu.gh>
2024-11-18 13:11:11 +0000

    Implemeted welcome page

---
+++ welcomepage.js
@@ -0,0 +1,110 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity, Image, Button } from 'react-native';
+
+export default function App() {
+  const handlePress = () => {
+    alert ("Skip clicked!");
+  };
+
+  const handleNavigation = () => {
+    alert ("Navigation done!");
+  };
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={handlePress} style = {styles.skipContainer}>
+      <Text style = {styles.skipText}> Skip </Text>
+      </TouchableOpacity>
+
+      <View style={styles.welcomeContainer}>
+        <Text style = {styles.welcomeText}> Welcome </Text>
+      </View>
+
+      <Image style = {styles.imagePositioning} 
+        source ={require('./assets/images/welcome_cats.png')}  />
+
+      <View style = {styles.greetingContainer}>
+        <Text style = {styles.greetingText}> Welcome to your PickitPickEat for vendor expertise. </Text>
+        <Text style = {styles.greetingText2}>  Join us and start your journey today. </Text> 
+      </View>
+
+      <View style = {styles.buttonSpace}>
+      <TouchableOpacity onPress={handleNavigation} 
+      style = {styles.greenButton}> 
+      <Text style = {styles.nextText}> Next </Text>
+      </TouchableOpacity>
+      </View>
+      
+      <StatusBar style= {styles.statusBar}
+      backgroundColor='green' />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: 60,
+    paddingLeft: 20,
+    paddingRight: 20,
+    flexDirection: "column"
+  },
+  skipContainer:{
+    flexDirection: "row",
+    height: "3%",
+    width: "100%",
+    alignContent: "flex-end",
+    justifyContent: "flex-end",
+    paddingTop: 1
+  },
+  skipText: {
+    fontSize: 18,
+    color: "#228B22",
+    fontWeight: 500
+  },
+  welcomeContainer: {
+    height: "3%",
+    width: "100%",
+    flexDirection: "row",
+    alignContent: "center",
+    justifyContent: "center",
+    top: 20
+  },
+  welcomeText: {
+    fontWeight: 500,
+    fontSize: 20,
+  },
+  imagePositioning: {
+    top: 180,
+  },
+  greetingContainer: {
+    top: '40%',
+  },
+  greetingText: {
+    color: "grey",
+    top: 50
+  },
+  greetingText2: {
+    color: "grey",
+    top: 50,
+    textAlign: "center"
+  },
+  buttonSpace: {
+    top: 390
+  },
+  greenButton: {
+    backgroundColor: '#228B22',
+    width: "80%",
+    alignSelf: "center",
+    height: 50,
+    borderRadius: 5,
+    padding: 12
+  },
+  nextText: {
+    color: 'white',
+    fontSize: 18,
+    alignSelf: "center",
+    fontWeight: "bold"
+  }
+});
