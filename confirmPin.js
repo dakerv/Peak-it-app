68a49d77083dbe819fd2ee924368d4bd43a0b764
Kendall <vedaker@st.ug.edu.gh>
2024-12-05 14:04:55 +0000

    Added email input button

--- confirmPin.js
+++ confirmPin.js
@@ -10,9 +10,33 @@ export default function confirmPin ({ navigation }) {
     }
     return(
         <View style={styles.container}>
-        <View style = {styles.logoContainer}>
+          <View style = {styles.logoContainer}>
             <Image source = {require ('./assets/images/Logo SVG 1.png')} /> 
             <Image source = {require ('./assets/images/PickEAT PickIT.png')} />
+          </View>
+
+          <View style = {styles.introContainer}>
+            <Text style = {styles.personalInfo}> Confirm Pin </Text>
+            <Text style = {styles.introText2}> To continue, kindly enter your email </Text>
+          </View>
+
+          <View style={[styles.emailContainer, texthighlight === 'email' && styles.textHighlightFeatures]}>
+        
+          <MaterialIcons
+          name='mail'
+          color={'lightgray'}
+          size={24}>
+          </MaterialIcons>
+
+          <TextInput
+          style = {styles.emailInput}
+          onChangeText = {onChangeEmail}
+          value = {email}
+          placeholder='Enter email'
+          onFocus={() => setTextHighlight ('email')}
+          onBlur={() => setTextHighlight (null)}>
+          </TextInput>
+
         </View>
         </View>
     )
