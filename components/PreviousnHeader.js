b3384305ab6aadef2e43c79269951e1f6d3f3607
Kendall <vedaker@st.ug.edu.gh>
2025-01-16 17:08:06 +0000

    Made a useless component

--- components/PreviousnHeader.js
+++ components/PreviousnHeader.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import SkipComponent from "./SkipComponent";
 
 const PreviousnHeader = props => {
     return (
@@ -14,13 +15,26 @@ const PreviousnHeader = props => {
           <Text style = {styles.pageHeader}> {props.pageHeading} </Text>
         </View>
   
-        <SkipComponent function = {handlePress} />
-  
-        <View style = {styles.greenBackground}>
+        <SkipComponent />
+
   
         </View>
-      </View>
     )
 }
+
+const styles = StyleSheet.create ({
+    backspacePlacement: {
+        top: '3%',
+        width: '100%',
+        flexDirection: 'row',
+        alignContent: 'center',
+    },
+    pageHeader: {
+        alignSelf: 'center',
+        fontWeight: 600,
+        fontSize: 18,
+        marginLeft: '26%'
+    },
+})
 
 export default PreviousnHeader;
