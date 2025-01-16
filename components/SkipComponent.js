bf87d05198a30426efaa95c54c83426f0aef36f4
Kendall <vedaker@st.ug.edu.gh>
2025-01-16 15:55:33 +0000

    Added and set up my new component

---
+++ components/SkipComponent.js
@@ -0,0 +1,14 @@
+import react from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const SkipComponent = props => {
+    return (
+
+    )
+}
+
+const styles = StyleSheet.create ({
+    container: {
+        
+    }
+})
